5d239a7ffb9448a471b0fd37d3fab9666b820066
IvanGMtz <navi.garces24@gmail.com>
2023-06-02 16:26:25 -0500

    feat: :sparkles: Excercise 3

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
 
-$planetas= array("Sol", "Mercurio", "Venus", "Tierra", "Marte", "Jupiter", "Saturno", "Urano", "Neptuno");
+$planetas= array("Sol" =>false, "Mercurio"=>false, "Venus"=>true, "Tierra"=>true, "Marte"=>true, "Jupiter"=>false, "Saturno"=>false, "Urano"=>false, "Neptuno"=>false);
 echo <<<HTML
     <br><a href="index.html">Volver</a><br>
     HTML;
 
     // print_r(count($planetas));
-    $Cantidad=array_values($_POST)[0];
-    $SistSolar=array_fill(0, $Cantidad, "Deshabilitado"); 
-    print_r($SistSolar);
-?>
+    $Filtro= array_values($_POST)[0];
+    if($Filtro=="true"){
+        $Resultado=array_filter($planetas, function($v, $k) {
+            return $v == true;
+        }, ARRAY_FILTER_USE_BOTH);
+    }else{
+        $Resultado=array_filter($planetas, function($v, $k) {
+            return $v == false;
+        }, ARRAY_FILTER_USE_BOTH);
+    };
+    
+
+    print_r(array_keys($Resultado));

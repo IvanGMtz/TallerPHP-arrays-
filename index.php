bbc6ec45c9af02dcaca1ad07a03a9cbd83c02d3e
IvanGMtz <navi.garces24@gmail.com>
2023-06-04 19:10:58 -0500

    feat: :sparkles: Excercise 6

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
     echo <<<HTML
     <br><a href="index.html">Volver</a><br>
     HTML;
-    echo "<h3>Masa Total</h3>";
-    $TotalMasa= array_sum($_POST);
-    print_r(number_format($TotalMasa, 2));
+
+    $planetas= array("sol", "mercurio", "venus", "tierra", "marte", "jupiter", "saturno", "urano", "neptuno");
+
+    $Planeta= strtolower($_POST["planeta"]);
+    if (array_key_exists($Planeta, array_flip($planetas))){
+        echo "Si existe el planeta {$Planeta}";
+    }else{
+        echo "No existe el planeta {$Planeta}";
+    };

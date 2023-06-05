17550e0d44afd6a351aa21de4243cf327b1e8bd2
IvanGMtz <navi.garces24@gmail.com>
2023-06-04 19:18:45 -0500

    feat: :sparkles: Excercise 7

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <br><a href="index.html">Volver</a><br>
     HTML;
 
-    $planetas= array("sol", "mercurio", "venus", "tierra", "marte", "jupiter", "saturno", "urano", "neptuno");
-
-    $Planeta= strtolower($_POST["planeta"]);
-    if (array_key_exists($Planeta, array_flip($planetas))){
-        echo "Si existe el planeta {$Planeta}";
+    $TipoNaves= array("sobrevuelo", "orbiter", "atmosferica", "penetradora", "observatorio", "comunicaciones", "lander", "rover");
+    //Tipos de Naves según Community Dereum Labs
+    $nave= strtolower($_POST["nave"]);
+    if (array_key_exists($nave, array_flip($TipoNaves))){
+        echo "Si existe la nave {$nave}";
     }else{
-        echo "No existe el planeta {$Planeta}";
+        echo "No existe la nave {$nave}";
     };

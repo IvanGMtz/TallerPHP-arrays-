616bee88ef24cbde579096c43d1b24d48208519e
IvanGMtz <navi.garces24@gmail.com>
2023-06-04 20:40:38 -0500

    feat: :sparkles: Excercise 13

--- index.php
+++ index.php
@@ -3,16 +3,9 @@
     echo <<<HTML
     <br><a href="index.html">Volver</a><br>
     HTML;
-
-    $Galaxia= array(
-        array("Sol", "Mercurio", "Venus", "Tierra", "Marte"),
-        array("Marte", "Jupiter", "Saturno", "Urano", "Neptuno"),
-        array("Venus", "Tierra", "Marte", "Jupiter", "Saturno"),
-        array("Sol", "Mercurio", "Saturno", "Urano", "Neptuno")
-    );
-    echo "<h3>Planetas en Común</h3>";
-    echo "<br>";
-    $array1 = $Galaxia[$_POST["SisSolar"]];
-    $array2 = $Galaxia[$_POST["SisSolar2"]];
-    $resultado = array_diff($array1, $array2);
-    print_r(array_values($resultado));
+    $planetas= array("Sol", "Mercurio", "Venus", "Tierra", "Marte", "Jupiter", "Saturno", "Urano", "Neptuno");
+    if($_POST["orden"]=="0"){
+        print_r($planetas);
+    }else{
+        print_r(array_reverse($planetas));
+    };

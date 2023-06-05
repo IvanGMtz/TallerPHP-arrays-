75e2ee9e841f9cf3e2afff80402e708ee846fd28
lñsalk <lsalk@gmail.com>
2023-06-05 16:58:32 -0500

    feat: :sparkles: Excercise 12

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
     echo <<<HTML
     <br><a href="index.html">Volver</a><br>
     HTML;
-    $planetas= array("Sol", "Mercurio", "Venus", "Tierra", "Marte", "Jupiter", "Saturno", "Urano", "Neptuno");
-    if($_POST["orden"]=="0"){
-        print_r($planetas);
-    }else{
-        print_r(array_reverse($planetas));
-    };
+    $planetas=[
+        "tierra"=>["satelites"=>["Luna"]],
+        "marte"=>["satelites"=>["Fobos", "Deimos"]],
+        "venus"=>["satelites"=>["Tritón", "Talasa"]]
+    ];
+    $planeta=array_pop($_POST);
+    array_push($planetas[$planeta]["satelites"], array_values($_POST)[0], array_values($_POST)[1], array_values($_POST)[2]);
+    echo "<pre>";
+    print_r(array_unique($planetas[$planeta]["satelites"]));
